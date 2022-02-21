32efc1043df7fc74b2e91d5d3701466c0d28bd33
attribbuttes <electronic.circus@gmail.com>
2022-02-20 21:53:58 -0300

    cosas css

--- app.js
+++ app.js
@@ -58,7 +58,11 @@ app.use("/metaverso", metaRoutes);
 const footRoutes = require ("./routes/footRoutes");
 app.use("/foot", footRoutes);
 
-
+//404//
+app.use((req, res, next) => {
+    res.status(404).render('404-page');
+    next();
+});
 
 
 
