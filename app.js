bc9a358224211722f10c477d46ca8a4de6c48e9f
attribbuttes <electronic.circus@gmail.com>
2022-03-05 22:35:47 -0300

    product detail ok

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const publicPath = path.resolve(__dirname, './public');
 app.use( express.static(publicPath) );
 
 app.listen(3080, () => {
-    console.log("Servidor corriendo en el puerto 3080")
+    console.log("Servidor corriendo en tu cola")
 });
 
 
