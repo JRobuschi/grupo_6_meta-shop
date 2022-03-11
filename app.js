106ecd454b5c15dc85a630564800659cd0b97764
Robuschi Juan Cruz <juanrobuschi5@gmail.com>
2022-03-11 19:14:01 -0300

    nuevo

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const publicPath = path.resolve(__dirname, './public');
 app.use( express.static(publicPath) );
 
 app.listen(3080, () => {
-    console.log("Servidor corriendo en tu cola")
+    console.log("Servidor corriendo")
 });
 
 
