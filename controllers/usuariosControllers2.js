34d2860c86074f1892ceee9b2d1937d198a9085c
attribbuttes <electronic.circus@gmail.com>
2022-04-06 21:37:53 -0300

    arreglando login y register

---
+++ controllers/usuariosControllers2.js
@@ -0,0 +1,180 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname,'../data/users.json');
+const users = JSON.parse(fs.readFileSync(filePath, {encoding: 'utf8'}))
+const bcryptjs = require('bcryptjs');
+const { validationResult, body } = require('express-validator');
+const check = require('express-validator').check;
+const User = require ('../models/User');
+
+const usuariosControllers = {
+    index: (req,res) => {
+        return res.render('users/iniciarSesion');  	
+    },
+
+    login: (req, res) => {
+        return res.render ('users/login'); //vista del login
+    },
+
+    register: (req, res) => {
+        return res.render ('users/register');
+    },
+
+    processRegister: (req, res) => {
+       const resultValidation = validationResult(req);
+        
+       if (resultValidation.errors.length > 0 ) {
+             res.render('./users/register', { //nombre de la vista de registro
+                errors: resultValidation.mapped(),
+                oldData: req.body
+            });
+            
+        }
+
+        let userInDB = User.findByField('email', req.body.email);
+
+        if (userInDB) {
+            res.render('./users/register', {
+            errors: {
+                email: {
+                    msg: 'este mail ya esta registrado'
+                }
+            },
+            oldData: req.body
+        });
+
+        }
+
+        
+        let userToCreate = {
+            ...req.body,
+            password: bcryptjs.hashSync(req.body.password, 10),
+            avatar: req.file.filename
+        }
+
+        
+        let userCreated = User.create(userToCreate); //crea y redirije a login
+        return res.redirect('/users/login')
+    },
+
+
+    login: (req, res) => {
+        return res.render('users/login');
+    },
+
+    processLogin: (req, res) => {
+        let userToLogin = User.findByField('email', req.body.email);
+        
+        if (userToLogin){
+            let isOkThePassword = bcryptjs.compareSync(req.body.password, userToLogin.password);
+            if(isOkThePassword) {
+                return res.redirect ('/users/userProfile')
+            }
+            return res.render('users/login', {
+                errors: {
+                    email:{
+                        msg: 'los datos son incorrectos'
+                    }
+                }
+            })
+        }
+        
+        return res.render('users/login', {
+            errors: {
+                email:{
+                    msg: 'no se encuentra el usuario'
+                }
+            }
+        })
+    
+    },
+
+
+    
+
+    create: (req,res) => {
+        const newUser =  req.body;
+		const newUserImage = req.file;
+	
+		if (req.file && newUserImage.size < 3145728) {
+			
+		usuariosControllers.createNewUser(newUser,newUserImage)	
+		
+		users.push (newUser)
+
+		usuariosControllers.dbReWrite()
+
+		res.redirect ('/users')
+
+	} else if (req.file && newUserImage.size > 3145729) {
+		res.send('El archivo es demasiado pesado')
+	} else {
+		res.send ('No adjuntaste ninguna imagen')
+	}
+       
+        
+
+       
+
+        
+    },
+    /*profile: (req,res) =>{
+        const idToFind = req.params.id
+        const user = users.find (p => p.id == idToFind)
+
+        return res.render ('users/perfil', {user})
+    },*/
+
+    
+
+    edit: (req,res) =>{
+        const idToFind = req.params.id
+        const usuario = users.find (p => p.id == idToFind)
+        
+         res.render ('users/editarPerfil', {usuario})
+    },
+    update: (req,res) =>{
+        const idToFind = req.params.id
+        const productIndex = users.findIndex(user => user.id == idToFind)
+        const editedProduct = req.body;
+
+        users[productIndex].nombre = editedProduct.nombre;
+        users[productIndex].apellido = editedProduct.apellido;
+        users[productIndex].email = editedProduct.email;
+        if (req.body.pdtCategori == ''){
+            users[productIndex].pdtCategori = users[productIndex].pdtCategori;
+        }else{
+            users[productIndex].pdtCategori = editedProduct.pdtCategori
+        }
+        users[productIndex].pdtDescription = editedProduct.pdtDescription
+        if(req.file) {
+            users[productIndex].pdtImg = req.file.filename;
+        }
+        controllers.dbReWrite()
+
+        return res.redirect('/users')
+    },
+    logout: (req,res) =>{
+        res.clearCookie('email');
+        req.session.destroy();
+        return res.redirect("/users")
+    },
+
+    dbReWrite() { 
+		fs.writeFileSync(filePath, JSON.stringify(users, null, 2))
+	},
+    createNewUser: function (newUser,newUserImage) {
+
+		newUser.id = usuariosControllers.asignIdToUser();
+		newUser.image = newUserImage.filename;
+		
+		
+	},
+	asignIdToUser: function () {
+		return users[users.length -1].id +1;
+	}
+    
+
+}
+
+module.exports = usuariosControllers;
---
+++ routes/users2.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var router = express.Router();
+const upload = require ("../middlewares/multerUser");
+
+
+const path = require('path');   
+const multer = require ('multer')
+
+const { body } = require ('express-validator'); // la variable body de validator en otros videos se llama check
+const validations = [ //campos a validar y metodo de validacion// estos formularios no pueden estar vacios
+    body ('email').notEmpty().withMessage('debe ingresar un mail').bail().isEmail().withMessage('debe ser fromato email'),
+    body ('password').notEmpty().withMessage('debe ingresar un password'),
+    body ('usuarios').custom((value, { req })=> {
+        let file = req.file;
+        let acceptedExtensions = ['.jpg', '.png', '.gif'];
+        
+        if (!file) {
+            throw new Error('debes subir una imagen')
+        } else {
+            let fileExtension = path.extname(file.originalname);
+        if (!acceptedExtensions.includes(fileExtension)){
+            throw new Error (`las extensiones permitidas son ${acceptedExtensions.join(',')}`);
+        }
+    }
+        return true;
+    }),
+   
+];
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=> {
+        cb(null, './public/images/usuarios');
+    },
+    filename: (req, file, cb)=> {
+    let filename = `${Date.now()}_img${path.extname(file.originalname)}`;
+    cb(null, filename);
+}
+}) 
+
+const uploadFile = multer ({ storage});
+
+const usuariosControllers = require('../controllers/usuariosControllers');
+
+const uploadUser = require('../middlewares/multerUser');
+
+
+
+
+//const validationResult = require('express-validator').validationResult;
+const check = require('express-validator').check;
+
+//let {check, validationResult, body } = require('express-validator').validationResult; min 21.16
+
+//let guestMiddleware = require('../middlewares/guestMiddleware');
+
+/* GET users listing. */
+router.get('/', usuariosControllers.index);
+
+router.get('/login', usuariosControllers.login);
+router.post('/login', [
+    check('email').isEmail().withMessage('Email invalido'),
+    check('password').isLength({min: 4}).withMessage('minimo 4 digitos')
+], usuariosControllers.processLogin);
+
+
+
+
+router.get('/chek', function (req,res){
+    if(req.session.usuarioLogueado == undefined) {
+        res.send('no estas logueado, bo');
+    } else {
+        res.send('el usuario logueado es' + req.session.usuarioLogueado.email);
+
+    }
+})
+
+
+//CREATE
+//router.get('/', usuariosControllers.create);
+router.post('/', upload.single('image'), usuariosControllers.create);
+
+//GET
+//router.get('/:id/', usuariosControllers.profile); // lo saque x q me rompía el register
+
+router.get('/register', usuariosControllers.register) //min 21:19
+
+router.post('/register', uploadFile.single('usuarios'), validations, usuariosControllers.processRegister) //min 21:19
+
+
+
+//EDIT
+router.get('/:id/edit', usuariosControllers.edit);
+//router.put('/:id', usuariosControllers.update);
+router.put('/:id', uploadUser.single('image'), usuariosControllers.update);
+
+//LOGOUT(hay que configurarlo con cookies)
+router.get('/logout', usuariosControllers.logout);
+
+
+
+
+module.exports = router;
