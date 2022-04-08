5fa1ac4abfa87b58e43ed21879443f2bfeee0a43
attribbuttes <electronic.circus@gmail.com>
2022-04-08 17:17:55 -0300

    login en proceso

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-var session = require ('express-session');
+const session = require ('express-session');
 const app = express();
 const cookieParser = require('cookie-parser');
 
