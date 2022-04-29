c87d6c7ee614f8c14bcb34f33fd4fffe29d7541e
Robuschi Juan Cruz <juanrobuschi5@gmail.com>
2022-04-29 14:23:42 -0300

    change

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const controllers = require("../controllers/mainControllers");
 
+//Falta completar, PERO SI SE AGREGAN HAY QUE MODIFICAR LAS RUTAS!!!
+
+module.exports = router;
